4fea981496185bb60be2c0544e3f50689dfa843a
Leonardo <programador2@uvp.mx>
2025-05-07 14:33:39 -0600

    Snakcbar Dialogs

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -25,8 +25,13 @@ const appMenuItems = <MenuItem>[
       link: '/cards',
       icon: Icons.credit_card),
   MenuItem(
-      title: 'ProgressIndicators',
+      title: 'Progress Indicators',
       subTitle: 'Generales y controlados',
       link: '/progress',
       icon: Icons.refresh_rounded),
+  MenuItem(
+      title: 'Snackbars y Dialogos',
+      subTitle: 'Indicadores en pantalla',
+      link: '/snackbar',
+      icon: Icons.info_outline_rounded),
 ];
